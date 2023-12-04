b4eb383921af3684f37d748f0452bf8a47dadafc
loveravocado <makaron-ayako@docomo.ne.jp>
2023-12-04 14:31:34 +0900

    DisplayFriend

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
-import { useEffect, useState, useContext } from 'react';
+import { useEffect, useState, useContext, useRef  } from 'react';
 import { collection, getDocs, getFirestore, where, query } from "firebase/firestore"; 
 import { auth, provider, username } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
@@ -10,33 +10,18 @@ import { db } from "../firebase";
 import { UserName } from './Home'; 
 
 
+export default function Friend(prpos){
+  const [username, setUsername] = useState()
 
-export default function Friend() {
-  const [saving, setSaving] = useState([]);
-
-  const [username, setUsername] = useState("ayako");
-  
-
-  console.log(username)
-  useEffect(() => {
-    (async()=>{ 
-      onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setUsername(user.displayName);
-            }
-        })
-
-    const savingdata = query(collection(db, "saving"), where("name", "!=", {username}));
-    const querySnapshot = await getDocs(savingdata);
-  
-    querySnapshot.forEach((doc) =>{ 
-      setSaving((saving)=>[...saving, doc.data()]);
-    });
-      
-    })()
-  }, [])
-
-
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log("1")
+        setUsername(user.displayName)
+        console.log({username});
+        }
+    }
+    
+  );
   return (
     <div>
         <Header name="Friend"/>
@@ -44,17 +29,59 @@ export default function Friend() {
         <div className='friend'>
           <div className='displayfriend'>
             <h1>友達一覧</h1>
-            {/* <div> {username ? (auth.currentUser.displayName):("")}</div> */}
-            {saving.map((saving)=> ( 
-              <div key={saving.name }>
-                <div>{saving.name}</div>
-                <div>{saving.month}</div>
-                <div>{saving.amount}</div>
-              </div>
-            ))}
+            {username ? (
+            <>
+            <FriendData  name={username}/>
+            </>
+            ) :("")}
+
+            
+            
           </div>
         </div>
         <Footer />
     </div>
   )
+  
 }
+
+function FriendData({name}){
+  
+  const [saving, setSaving] = useState([]);
+  console.log("2")
+  console.log(name)
+  
+  
+
+  useEffect(() => {
+    
+    const f = async() => {
+      
+    const savingdata = query(collection(db, "saving"), where("name", "!=", name));
+    console.log("3")
+       
+      const querySnapshot = await getDocs(savingdata);
+    
+      querySnapshot.forEach((doc) =>{ 
+        setSaving((saving)=>[...saving, doc.data()]);
+      });
+    };
+    f();
+    
+  }, []);
+  return(
+    <>
+    {saving.map((saving)=> ( 
+      <div key={saving.name }>
+        <div>{saving.name}</div>
+        <div>{saving.month}</div>
+        <div>{saving.amount}</div>
+      </div>
+    ))}
+    </>
+  )
+    
+  
+}
+
+
